7b0d647b2e13d1f2f0e8efc6210a931eb49f083c
Aaron Le <stinkypants@Aarons-MacBook-Air.local>
2025-03-15 21:26:26 -0500

    updated macro class to store nutrition information

---
+++ fitness/lib/model/macro.dart
@@ -0,0 +1,15 @@
+class Macro{
+  String food;
+  int calories;
+  int protein;
+  int carbs;
+  int fats;
+
+  Macro({
+    required this.food,
+    required this.calories,
+    required this.protein,
+    required this.carbs,
+    required this.fats,
+  });
+}
